7618851ca3743a93d19a8bf67db5bfab28539651
Kevin Cruz <kcruz538@gmail.com>
2015-01-14 00:32:01 -0600

    exercise controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114042035) do
+ActiveRecord::Schema.define(version: 20150114061004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: true do |t|
+    t.string   "name"
+    t.integer  "sets"
+    t.integer  "reps"
+    t.integer  "workout_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "exercises", ["workout_id"], name: "index_exercises_on_workout_id", using: :btree
 
   create_table "workouts", force: true do |t|
     t.datetime "date"
